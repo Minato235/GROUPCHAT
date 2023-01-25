9faf438472cb130d8165a688138ccdc79102e78a
Minato <ankamasish@gmail.com>
2023-01-25 18:47:03 +0530

    task 9 completed

--- frontEnd/afterLogin.js
+++ frontEnd/afterLogin.js
@@ -8,6 +8,8 @@ function showMessageInScreen(chat,name){
     div.innerHTML+=`<h4>${name}: ${chat}</h4>`
 }
 
+
+
 document.getElementById("send").addEventListener("click", () => {
     token=localStorage.getItem("token");
     name1=localStorage.getItem("name1");
@@ -17,12 +19,13 @@ document.getElementById("send").addEventListener("click", () => {
         name1
     }
     console.log(obj)
+   
     axios.post("http://localhost:3000/user/sendMessage",obj,{
         headers: {
             "Authorization": token
         }
     }).then(result => {
-     
+       
         console.log(result)
         window.location.reload()
 
@@ -32,11 +35,40 @@ document.getElementById("send").addEventListener("click", () => {
 })
 window.addEventListener("DOMContentLoaded",()=>{
     console.log("inside DOMContentLoaded")
-    let lastMessage=localStorage.getItem("lastMessage");
-    
-    axios.get(`http://localhost:3000/user/getAllMessages?lastMessage=${lastMessage}`).then(responce=>{
+    let lastMessageId;
+    if(!localStorage.getItem("msgArray")){
+          lastMessageId=0
+    }else
+    {   
+        let msgArray=JSON.parse(localStorage.getItem("msgArray"));
+        lastMessageId = msgArray[msgArray.length-1].id;
+    }
+  
+    axios.get(`http://localhost:3000/user/getAllMessages?lastMessage=${lastMessageId}`).then(responce=>{
         let msg=responce.data.result;
-        msg.forEach(element=>{
+         console.log('msg',msg);
+         if(msg.length>0){
+             if (!localStorage.getItem("msgArray")) {
+               let msgArray = JSON.stringify(msg);
+               localStorage.setItem("msgArray", msgArray);
+               msg.forEach((element) => {
+                 console.log(element);
+                 showMessageInScreen(element.messageText, element.name);
+               });
+             } else {
+               let msgArray = JSON.parse(localStorage.getItem("msgArray"));
+               console.log("msgArray", msgArray);
+               msgArray.push(...msg);
+               console.log("newMsgArray", msgArray);
+               localStorage.setItem("msgArray", JSON.stringify(msgArray));
+               
+             }
+         }
+        
+         
+          
+        let msgArray = JSON.parse(localStorage.getItem("msgArray"));
+        msgArray.forEach(element=>{
             console.log(element)
            showMessageInScreen(element.messageText,element.name)
         })
